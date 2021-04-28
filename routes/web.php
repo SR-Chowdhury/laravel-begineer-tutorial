12bbcb64519e2d4e6b4ad31872d74adf1fe24ae2
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-04-28 22:44:30 +0600

    Basic Routing in Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    // echo 'It is about page';
+    return view('introduction/about');
+});
+
+Route::get('/contact', function() {
+    // echo 'It is Contact page';
+    return view('introduction/contact');
+});
+
+// Route Prefixes
+Route::prefix('superstar')-> group(function() {
+    Route::get('/me', function () {
+        echo "This is Me"; // Run in the browser: localhost:xxxx/supserstar/me
+    });
+    Route::get('/you', function () {
+        echo "This is you"; // Run in the browser: localhost:xxxx/supserstar/you
+    });
+});
+
